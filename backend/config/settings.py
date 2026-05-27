caf35eb14632ecbb097cad37defe32a60c2de422
Divya Krupa <divyakrupabonam@gmail.com>
2026-05-27 16:46:05 +0530

    remove csrf auth conflicts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,5 +147,8 @@ CSRF_TRUSTED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+      'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
