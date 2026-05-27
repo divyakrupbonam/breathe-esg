dd7410b5882a21f762c681698e1e7d3a35b15dfc
Divya Krupa <divyakrupabonam@gmail.com>
2026-05-27 14:32:13 +0530

    update Django middleware and deployment settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,10 +49,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
-    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
