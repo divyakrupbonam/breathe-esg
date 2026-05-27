999b1f88dd1b737bf2e6db16e8f216c765739235
Divya Krupa <divyakrupabonam@gmail.com>
2026-05-27 23:16:16 +0530

    fix csrf railway + vercel origin issue

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -9,7 +9,7 @@ function Login({ setToken }) {
   const login = async () => {
     try {
       const res = await axios.post(
-        "https://breathe-esg-production-83b0.up.railway.app/api/token/",
+        "https://breathe-esg-production-d49f.up.railway.app/api/token/",
         { username, password }
       );
 
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://breathe-esg-production-83b0.up.railway.app",
+    baseURL: "https://breathe-esg-production-d49f.up.railway.app",
 });
 
-// automatically attach token
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
