195bcda05d21a3d966b384a50dc470b8eec43af9
Divya Krupa <divyakrupabonam@gmail.com>
2026-05-27 16:33:04 +0530

    fix upload authentication with jwt

--- backend/ingestion/views.py
+++ backend/ingestion/views.py
@@ -6,16 +6,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import DataSource
 from companies.models import Company
 from emissions.models import EmissionRecord
 
 from .utils import normalize_unit, calculate_emission
 
-@method_decorator(csrf_exempt, name='dispatch')
+
 class UploadCSVView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request):
