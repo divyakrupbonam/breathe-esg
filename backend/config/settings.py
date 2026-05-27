039d717935f87efc9bea68a5b38a6b7668f0eb61
Divya Krupa <divyakrupabonam@gmail.com>
2026-05-27 16:07:10 +0530

    fix csrf trusted origins for vercel

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,6 +138,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://breathe-esg-zeta.vercel.app/",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
